794b02a84000d9ffbcf0276090c31d9ae7a0e1d8
Pedro Schneider <pedroschneider@outlook.com>
2020-08-03 19:20:48 -0300

    refactored

--- refactor.c
+++ refactor.c
@@ -1,66 +1,57 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-// HACK: should change dfs's signature and change definition for edges[C][C]
-//     -- void dfs(int* castles, int** edges, int run, int castle);
-// and castles[C].
-#define C 8
+void dfs(const int *C, int *best, int* castles, int **edges, int depth, int castle) {
+  if (*best < depth) { *best = depth; }
 
-int best;
-// should edges and castles be copies? or should all runs accounts in the same?
-void dfs(int castles[C], int edges[C][C], int run, int castle) {
-  if (best < run) { best = run; }
-  int cost;
-  for (int i = 0; i < C; ++i)
+  int *next_castles = (int*) malloc(sizeof(int) * *C * *C);
+  memcpy(next_castles, castles, sizeof(int) * *C * *C);
+
+  int cost = 0;
+  for (int i = 0; i < *C; ++i) {
     if (edges[castle][i] && castle != i) {
-      //printf("%d: %d - %d\n", castle, i, run);
+      //printf("%d: %d - %d\n", castle, i, depth);
       if (castles[castle] - ((2*castles[i]) + 50) >= 0) {
         cost = (2*castles[i]) + 50;
         castles[castle] -= cost;
-        dfs(castles, edges, run + 1, castles[i]);
+        dfs(C, best, next_castles, edges, depth + 1, next_castles[i]);
       }
     }
+  }
+
 }
 
 int main() {
-  int S; /*C = 0*/; int E;
-  int trash;
-  best = 0;
-  // scanf("%d %d %d", &S, &C, &E);
-  scanf("%d %d %d", &S, &trash, &E);
-  // printf("%d %d %d\n", S, C, E); // DEBUG!
+  /// READ KING'S CASTLE'S ATTRIBUTES.
+  int S; int C; int E;
+  int best = 0;
+  scanf("%d %d %d", &S, &C, &E);
+  // printf("%d %d %d\n", S, C, E);
 
-  int castles[C];
-  castles[0] = S; // account for the king!
+  /// SAVE EACH CASTLE'S ARMY.
+  int *castles = (int*) malloc(sizeof(int) * C);
+  castles[0] = S;
   int castle_id; int army;
   for (int i = 0; i < C; ++i) {
     scanf("%d %d", &castle_id, &army);
     castles[castle_id] = army;
-    // printf("%d %d\n", castle_id, army); // DEBUG!
+    // printf("%d %d\n", castle_id, army);
   }
 
-  int edges[C][C];
+  /// READING POSSIBLE PATHS (EDGES).
+  int **edges = (int**) malloc(sizeof(int) * C * C); // <<< (BUG?)
   int departure; int arrivals;
   memset(edges, 0, sizeof(int) * C * C);
   for (int i = 0; i < E; ++i) {
     scanf("%d %d", &departure, &arrivals);
     edges[departure][arrivals]++;
     // printf("[%d][%d] = %d\n", departure, arrivals, edges[departure][arrivals]);
-    // DEBUG!
   }
 
-  printf("  0 1 2 3 4 5 6 7 \n");
-  //printf("-----------------\n");
-  for (int i = 0; i < C; ++i) {
-    printf("%d|", i);
-    for (int j = 0; j < C; ++j) {
-      printf("%d ", edges[i][j]);
-    }
-    printf("\n");
-  }
-
-  dfs(castles, edges, 0, 0);
-  // printf("best: %d\n", best);
+  /// COMPUTE THE ANSWEAR AND PRINT IT OUT TO STDOUT.
+  dfs(&C, &best, castles, edges, 0, 0);
+  printf("best: %d\n", best);
 }
 
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+gcc -O3 refactor.c && ./a.out < 0.in
+
