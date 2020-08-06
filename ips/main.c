7769aa0942a87ad496d47f4bed8db1647202e62f
Pedro Schneider <pwschneider@outlook.com>
2020-08-06 09:50:17 -0300

    add ip interval merge solution

---
+++ ips/main.c
@@ -0,0 +1,50 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct interval {
+  int64_t lower;
+  int64_t upper;
+} interval;
+
+int comp(const void *a, const void *b) {
+  interval *p = (interval*) a;
+  interval *q = (interval*) b;
+  return p->lower - q->lower;
+}
+
+int main() {
+  int64_t n;
+  scanf("%lld", &n);
+
+  interval *intervals = (interval*) malloc(sizeof(interval) * n);
+  int64_t lower; int64_t upper;
+  for (int i = 0; scanf("%lld-%lld", &lower, &upper) != EOF; ++i) {
+    interval *it = (interval*) malloc(sizeof(interval));
+    it->lower = lower;
+    it->upper = upper;
+    intervals[i] = *(it);
+  }
+
+  // sort the intervals by their lower bound.
+  qsort(intervals, n, sizeof(interval), comp);
+
+  int64_t current_lower = intervals[0].lower;
+  int64_t current_upper = intervals[0].upper;
+  int changed = 0;
+  for (int i = 1; i < n; ++i) {
+    if (current_upper >= intervals[i].lower) {
+      if (current_upper < intervals[i].upper) {
+        current_upper = intervals[i].upper;
+        changed = 1;
+      }
+    } else {
+      printf("%lld-%lld\n", current_lower, current_upper);
+      current_lower = intervals[i].lower;
+      current_upper = intervals[i].upper;
+      changed = 0;
+    }
+  }
+
+  if (changed) printf("%lld-%lld\n", current_lower, current_upper);
+}
---
+++ ips/run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+gcc main.c -O3 && ./a.out < 0.in && rm a.out
+
