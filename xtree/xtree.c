3e23a1d686eac8aa428bd12c983456b9f8b2e8a4
Pedro Schneider <pedroschneider@outlook.com>
2020-08-10 13:15:01 -0300

    spaces > tabs

--- xtree/xtree.c
+++ xtree/xtree.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 
 void explora(int gen, int *final_gen, int *total_val, int *qtd_nodes) {
-	int nfilhos, nvalores, val;
+  int nfilhos, nvalores, val;
   scanf("%d %d", &nfilhos, &nvalores);
 
-	while (nfilhos--)
-		explora(gen + 1, final_gen, total_val, qtd_nodes);
+  while (nfilhos--)
+    explora(gen + 1, final_gen, total_val, qtd_nodes);
 
-	while (nvalores--) {
+  while (nvalores--) {
     scanf("%d", &val);
-		(*total_val) += val;
-	}
-	if (gen > *final_gen)
-		*final_gen = gen;
+    (*total_val) += val;
+  }
+  if (gen > *final_gen)
+    *final_gen = gen;
 
-	(*qtd_nodes)++;
+  (*qtd_nodes)++;
 }
 
 int main() {
   int final_gen = 0; int total_val = 0; int qtd_nodes = 0;
-	explora(0, &final_gen, &total_val, &qtd_nodes);
-	printf("Quantidade de nodos: %d\n", qtd_nodes);
-	printf("Altura da arvore: %d\n", final_gen);
-	printf("Soma total dos nodos: %d\n", total_val);
+  explora(0, &final_gen, &total_val, &qtd_nodes);
+  printf("Quantidade de nodos: %d\n", qtd_nodes);
+  printf("Altura da arvore: %d\n", final_gen);
+  printf("Soma total dos nodos: %d\n", total_val);
 }
