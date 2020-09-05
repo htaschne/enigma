e269a5887779470fcd153798f654d6e698a7628a
Pedro Schneider <pedroschneider@outlook.com>
2020-09-05 02:16:20 -0300

    even faster but not enough

--- josephus/faster.c
+++ josephus/faster.c
@@ -3,42 +3,21 @@
 #include <string.h>
 #include <stdbool.h>
 
-bool done(char *str, int size) {
-  // def done(s):
-  //     return '1' not in s
-  for (int i = 0; i < size; ++i) {
-    if (str[i] == '1') {
-      return false;
-    }
-  }
-  return true;
-}
-
-bool simula(char *str, int size, int knife_position, int *jump) {
-  // # s=str, k=knife_position, j=jump_size
-  // def sim(s,k,j):
-  //   #print(s,k,j)
-  //   if done(s): return True
-  //   killpos = (k+j) % len(s)
-  //   else:
-  //     return False
-
-  if (done(str, size)) {
+bool simula(char *str, int size, int knife_position, int *jump, int ones) {
+  if (!ones) {
     return true;
   }
 
   int killpos = (knife_position + *jump) % size;
-  //   if s[killpos] == '1':
+
   if (str[killpos] == '1') {
-    //     s = s[:killpos] + s[killpos+1:]
-    //     return sim(s, killpos % len(s), j)
+    // if str were a linked list the remove at index would be O(1)
+    // currently it's O(n), where n = len(str)
     char *new_str = malloc(sizeof(char) * (size-1));
-    int a = 0;
-    for (int i = 0; i < size; ++i) {
-      if (i == killpos) continue;
-      new_str[a++] = str[i];
-    }
-    bool ret = simula(new_str, size-1, killpos % (size-1), jump); 
+    strncpy(new_str, str, killpos);
+    strncpy(((char*)new_str)+killpos, ((char*)str)+killpos+1, size-1-killpos);
+
+    bool ret = simula(new_str, size-1, killpos % (size-1), jump, ones - 1); 
     free(new_str);
     return ret;
   }
@@ -47,18 +26,6 @@ bool simula(char *str, int size, int knife_position, int *jump) {
 }
 
 int main() {
-  // initstr = sys.argv[1]
-  // print(initstr)
-  // 
-  // j = 0 # comeca jah pulando
-  // 
-  // res = False
-  // while not res:
-  //   j+=1
-  //   res = sim(initstr, 0, j)
-  // 
-  // print(j)
-
   char *buf = (char*) malloc(sizeof(char) * 255);
   gets(buf);
   int size = strlen(buf);
@@ -66,13 +33,15 @@ int main() {
   memcpy(in, buf, size);
   free(buf);
 
-  int jump = 0, knife_position = 0;
+  int jump = 0, knife_position = 0, ones = 0;
+
+  for (int i=0;i<size;++i) if (in[i]=='1') ones++;
 
   bool res = false;
   while (!res) {
     jump++;
-    res = simula(in, size, 0, &jump);
+    res = simula(in, size, 0, &jump, ones);
   }
 
-  printf("%d\n", jump);
+  printf("%d\n", --jump);
 }
--- josephus/main.c
+++ josephus/main.c
@@ -3,47 +3,44 @@
 #include <string.h>
 
 int pula(int pos, int cont, char* s, int size) {
-	while (cont) {
-		pos++;
-		if ( pos == size ) pos = 0;
-		if ( s[pos] != '+' ) cont--;
-	}
-	return pos;
+  while (cont) {
+    pos++;
+    if (pos == size) pos = 0;
+    if (s[pos] != '+') cont--;
+  }
+  return pos;
 }
 
 int simula(char *in, int len, int jump, int count) {
-	int pos = 0;
-	int vivos = len;
-
-	while (count > 0) {
-		pos = pula(pos, (jump+1)%vivos, in, len);
-		if ( in[pos] == '0' ) return 0;
-		in[pos] = '+';
-		count--;
-		vivos--;
-		pos = pula( pos, 1, in, len );
-	}
-	return jump;
+  int pos = 0;
+  int vivos = len;
+  while (count > 0) {
+    pos = pula(pos, (jump+1)%vivos, in, len);
+    if ( in[pos] == '0' ) return 0;
+    in[pos] = '+';
+    count--;
+    vivos--;
+    pos = pula(pos, 1, in, len);
+  }
+  return jump;
 }
 
 int main() {
-	char *buf = (char*) malloc(sizeof(char) * 255);
-	gets(buf);
-	int size = strlen(buf);
-	char *in = (char*) malloc(sizeof(char) * size);
-	memcpy(in, buf, size);
-	free(buf);
+  char *buf = (char*) malloc(sizeof(char) * 255);
+  gets(buf);
+  int size = strlen(buf);
+  char *in = (char*) malloc(sizeof(char) * size);
+  memcpy(in, buf, size);
+  free(buf);
 
-	int res, jump = 0;
-	int ones = 0;
-	char *current = (char*) malloc(sizeof(char) * size);
+  int res, jump = 0, ones = 0;
+  for (int i = 0; i < size; ++i) if (in[i] == '1') ones++;
+  char *current = (char*) malloc(sizeof(char) * size);
+  do {
+    memcpy(current, in, size);
+    res = simula(current, size, jump, ones);
+    jump++;
+  } while (res == 0);
 
-	for (int i = 0; i < size; ++i) if (in[i] == '1') ones++;
-	do {
-		memcpy(current, in, size);
-		res = simula(current, size, jump, ones);
-		jump++;
-	} while (res == 0);
-
-	printf("resposta: %d\n", res);
+  printf("%d\n", res);
 }
