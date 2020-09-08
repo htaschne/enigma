bdbf4d991399df83cd3d7306d0f426a1f8dc69c6
Pedro Schneider <pedroschneider@outlook.com>
2020-09-08 18:19:36 -0300

    dfs + priorityqueue should solve

--- scheduler/timer.cc
+++ scheduler/timer.cc
@@ -1,30 +1,29 @@
-
 #include <iostream>
 #include <memory>
 #include <vector>
 #include <stack>
 #include <map>
 #include <set>
+#include <queue>
+#include <cassert>
 
 using namespace std;
 
+class proc {
+public:
+  int time;
+  string name;
+  proc(string n, int t) {
+    name = n;
+    time = t;
+  }
+};
+
 // globals
-vector<vector<string>> order;
-map<string, vector<string>> dep;
+map<string, vector<string>> G;
 set<string> departures;
 set<string> arrivals;
 map<string, int> times;
-
-
-// FIXME: talvez isso devesse ser uma ordenacao topologica
-// ja que preenche a pilha da qual a gente retirna na ordem
-// em que foram inseridos os elemntos
-void dfs(string target, int depth) {
-  cout << depth << " " << target << endl;
-  order[depth].push_back(target);
-  for (auto &n : dep[target])
-    dfs(n, depth + 1);
-}
 
 int main() {
   int procs;
@@ -46,7 +45,7 @@ int main() {
     times[name_pos] = time_pos;
 
     // dependencies_map[first_process] += child_process
-    dep[name_pre].push_back(name_pos);
+    G[name_pre].push_back(name_pos);
     departures.insert(name_pre);
     arrivals.insert(name_pos);
   }
@@ -55,28 +54,38 @@ int main() {
   for (auto &pr : departures) {
     if (!arrivals.count(pr)) {
       roots.insert(pr);
-      break;
     }
   }
 
-  // for (auto &r : roots) cout << r.name << " " << r.time << endl;
-  for (auto &r : roots)
-    dfs(r, 0);
+  auto comp = [](proc a, proc b) { return a.time > b.time; };
+  priority_queue<proc, vector<proc>, decltype(comp)> pq (comp);
+  for (auto &root : roots)
+    pq.push(proc(root, times[root]));
 
-  // // dfs working proof!
-  // for (auto it = tt.begin(); it != tt.end(); ++it) {
-  //   auto procs = it->second;
-  //   for (auto ti = procs.begin(); ti != procs.end(); ++ti)
-  //     cout << it->first << " " << ti->name << endl;
-  // }
+  int ETA = 0, n = procs;
+  vector<proc> atuais;
+  while (!pq.empty()) {
+    // cout << pq.top().name << " " << pq.top().time << endl; pq.pop();
+    while (n--) {
+      // there's a free processor. put it to work!
+      proc top = pq.top(); pq.pop();
+      atuais.push_back(top);
 
+      // push children process into the priority queue.
+      for (auto &p : G[top.name])
+        pq.push(proc(p, times[p]));
+    }
 
-  // MIN
-  int N = procs;
-  stack<string> S;
-  cout << order.size() << endl;
-  for (int i = 0; i < order.size(); ++i) {
-    cout << i << endl;
+    assert(atuais.size() == procs);
+    auto bool_comp = [](proc a, proc b) { return a.time > b.time; };
+    int min_val = (min_element(atuais.begin(), atuais.end(), bool_comp))->time;
+
+    ETA += min_val;
+
+    // distribute min_val across all procs in atuais
+    // at least one of the cores will be released.
+
   }
 
 }
+
