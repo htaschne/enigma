f2198e1a671d14e2c29ba72744ed86e0718ce34c
Pedro Schneider <pedroschneider@outlook.com>
2020-09-12 13:07:12 -0300

    change default to C

--- josephus/run.sh
+++ josephus/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # C version
-# gcc -O3 main.c && ./a.out < 0.in
-g++ -O3 main.cc && time ./a.out < 1.in
+# g++ -O3 main.cc && time ./a.out < 1.in
+gcc -O3 main.c && time ./a.out < input
