f6eda1c095d36b79ef00580c4ecfd0e163bd5bdc
Pedro Schneider <pedroschneider@outlook.com>
2020-09-14 19:05:35 -0300

    simpler c++ version

--- ips/alt.cc
+++ ips/alt.cc
@@ -5,44 +5,29 @@
 int main() {
     using namespace std;
 
-    class interval {
-    public:
-        int64_t lower;
-        int64_t upper;
-        interval(int64_t a, int64_t b) {
-            lower = a; upper = b;
-        }
-    };
-
     // reading all intervals from input to a vector of intervals
-    vector<interval> intervals; int64_t lower; int64_t upper;
+    int64_t lower; int64_t upper;
+    vector<pair<int64_t, int64_t>> intervals;
     while (scanf("%lld-%lld", &lower, &upper) != EOF) {
-        interval it (lower, upper);
-        intervals.push_back(it);
+        intervals.push_back(make_pair(lower, upper));
     }
 
     // sort the intervals by their lower bound.
-    sort(intervals.begin(), intervals.end(), [] (interval a, interval b){
-        return a.lower < b.lower;
+    sort(intervals.begin(), intervals.end(), [](auto a, auto b) {
+        return a.first < b.first;
     });
 
-    // keep trying to join intervals. if it's not possible than print
-    // the current interval and start with the next one.
-    // i.e. [ [1-3], [3-10], [11-13], [11-14] ];
-    // will merge [1-10] and print and than do the same and print [11-14].
     bool changed = false;
-    auto current_lower = intervals[0].lower;
-    auto current_upper = intervals[0].upper;
-    for (int i = 1; i < (int) intervals.size(); ++i) {
-        if (current_upper >= intervals[i].lower) {
-            if (current_upper < intervals[i].upper) {
-                current_upper = intervals[i].upper;
+    auto current_lower = intervals[0].first;
+    auto current_upper = intervals[0].second;
+    for (size_t i = 1; i < intervals.size(); ++i) {
+        if (current_upper >= intervals[i].first &&
+           (current_upper = max(current_upper, intervals[i].second))) {
                 changed = true;
-            }
         } else {
             printf("%lld-%lld\n", current_lower, current_upper);
-            current_lower = intervals[i].lower;
-            current_upper = intervals[i].upper;
+            current_lower = intervals[i].first;
+            current_upper = intervals[i].second;
             changed = false;
         }
     }
--- ips/run.sh
+++ ips/run.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+# c++ command:
+# g++ -std=c++17 -O3 alt.cc && ./a.out < 0.in 20 && rm a.out
 gcc main.c -O3 && cat 0.in | ./a.out 0.in 20 && rm a.out
