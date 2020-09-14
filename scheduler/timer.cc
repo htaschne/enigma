016c46537c083a0cc92709aee20e2de88ceeda89
Pedro Schneider <pedroschneider@outlook.com>
2020-09-13 22:05:31 -0300

    error

--- scheduler/timer.cc
+++ scheduler/timer.cc
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <memory>
 #include <vector>
@@ -21,13 +22,13 @@ public:
 
 // globals
 map<string, vector<string>> G;
+map<string, int> times;
 set<string> departures;
 set<string> arrivals;
-map<string, int> times;
 
 int main() {
-  int procs;
-  scanf("# Proc %d", &procs);
+  size_t procs;
+  scanf("# Proc %ld", &procs);
   // printf("# Proc %d\n", procs);
 
   string pre, arr, pos;
@@ -44,10 +45,12 @@ int main() {
     int time_pos = stoi(pos.substr(pos_pos+1));
     times[name_pos] = time_pos;
 
-    // dependencies_map[first_process] += child_process
+    // dependencies graph. i.e: G[process] += child_process
     G[name_pre].push_back(name_pos);
     departures.insert(name_pre);
     arrivals.insert(name_pos);
+
+    // cout << name_pre << " -> " << name_pos << endl;
   }
 
   set<string> roots;
@@ -65,14 +68,15 @@ int main() {
   int ETA = 0, n = procs;
   vector<proc> atuais;
   while (!pq.empty()) {
-    // cout << pq.top().name << " " << pq.top().time << endl; pq.pop();
+    // error for object 0x9000000000000000: pointer being freed was not allocated
+    cout << pq.top().name << " " << pq.top().time << endl; pq.pop();
     while (n--) {
       // there's a free processor. put it to work!
-      proc top = pq.top(); pq.pop();
-      atuais.push_back(top);
+      proc tp = pq.top(); pq.pop();
+      atuais.push_back(tp);
 
       // push children process into the priority queue.
-      for (auto &p : G[top.name])
+      for (auto &p : G[tp.name])
         pq.push(proc(p, times[p]));
     }
 
@@ -84,8 +88,6 @@ int main() {
 
     // distribute min_val across all procs in atuais
     // at least one of the cores will be released.
-
   }
-
 }
 
