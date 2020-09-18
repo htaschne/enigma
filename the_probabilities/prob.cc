a212e90065d084d2e56af5d42ff93df04fb94d7b
Pedro Schneider <pedroschneider@outlook.com>
2020-09-18 16:45:15 -0300

    cpp version turned out to be more complicated than it should

--- the_probabilities/prob.cc
+++ the_probabilities/prob.cc
@@ -1,23 +1,79 @@
 
+#include <string>
+#include <fstream>
 #include <iostream>
 #include <vector>
-#include <tuple>
 #include <map>
 #include <set>
+#include <algorithm>
 
-int main() {
+using namespace std;
 
-  using namespace std;
+map<string, vector<string>> G;
+map<string, double> costs;
 
-  map<string, vector<string>> G;
+string parseInput(string filename) {
+    set<string> arr;
+    set<string> dep;
+    string line;
+    fstream input(filename);
+    while (getline(input, line)) {
+        string current = "";
+        vector<string> tokens;
 
-  string line;
-  set<tuple<string, vector<string>, int>> bag;
-  while (cin >> line) {
-  }
+        bool changed = false;
+        for (char ch : line) {
+            if (ch == ' ') {
+                tokens.push_back(current);
+                current = "";
+                changed = false;
+            } else {
+                changed = true;
+                current += ch;
+            }
+        }
+        if (changed) tokens.push_back(current);
 
-  vector<string> v;
-  auto tp = make_tuple(line, v, 0);
+        // b : ba 25 bb 35 b 40
+        auto pre = tokens[0];
+        dep.insert(pre);
+        for (size_t i = 2; i < tokens.size(); i = i + 2) {
+            G[pre].push_back(tokens[i]);
+            costs[tokens[i]] = (double) stoi(tokens[i+1]) / 100.0;
+            arr.insert(tokens[i]);
+        }
+    }
+    input.close();
 
-  return 0;
+    set<string> res;
+    set_difference(dep.begin(), dep.end(),
+                   arr.begin(), arr.end(), inserter(res, res.end()));
+
+    string root = *(res.begin());
+    return root;
+}
+
+double best = 0; string namebest;
+void explore(string target) {
+    if (G[target].size() == 0) {
+        if (costs[target] > best) {
+            best = costs[target];
+            namebest = target;
+        }
+        return;
+    }
+
+    for (auto &ch : G[target]) {
+        costs[ch] *= costs[target];
+        explore(ch);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    string filename = argc > 2 ? argv[1] : "0.in";
+    auto root = parseInput(filename);
+    costs[root] = 1.0;
+    explore(root);
+    cout << namebest << ": " << (best * 100) << "%" << endl;
+    return 0;
 }
---
+++ the_probabilities/run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+g++ -std=c++17 -O3 prob.cc && ./a.out 0.in && rm a.out
