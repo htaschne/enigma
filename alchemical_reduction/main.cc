42c8e36259618c54e8cceca439f10a1dd12ea40d
Pedro Schneider <pedroschneider@outlook.com>
2020-09-18 22:32:09 -0300

    add part II

--- alchemical_reduction/main.cc
+++ alchemical_reduction/main.cc
@@ -1,15 +1,14 @@
-
 #include <iostream>
 #include <stack>
+#include <set>
 #include <vector>
 
-using namespace std;
-
 int main() {
+  using namespace std;
 
-  auto react = [&] (vector<char> v) {
+  auto react = [&] (const vector<char> &v) {
     stack<char> st;
-    for (char ch; scanf("%c", &ch) != EOF;) {
+    for (auto &ch : v) {
       if (st.size() == 0) {
         st.push(ch);
         continue;
@@ -21,12 +20,33 @@ int main() {
         st.push(ch);
 
     }
-    return st.size() - 1;
+    return (int) st.size() - 1;
   };
 
-  vector<char> v;
-  for (auto& e : v) cin >> e;
+  auto filter = [&] (vector<char>& v, char needle) {
+    vector<char> new_v;
+    for (int i = 0; i < v.size(); ++i) {
+      if (v[i] == needle || v[i] == toupper(needle) || v[i] == tolower(needle)) continue;
+      new_v.push_back(v[i]);
+    }
+    return new_v;
+  };
 
-  printf("%ld\n", react(v));
+  vector<char> v; for (char ch; scanf("%c", &ch) != EOF;) v.push_back(ch);
+
+  // part I
+  printf("%d\n", react(v));
+
+  set<char> seen;
+  int best = numeric_limits<int>::max();
+  for (auto &ch : v) {
+    if (!seen.count(ch)) {
+      seen.insert(ch); seen.insert(toupper(ch)); seen.insert(tolower(ch));
+      auto current_v = filter(v, ch);
+      best = min(best, react(current_v));
+    }
+  }
+
+  printf("%d\n", best);
   return 0;
 }
