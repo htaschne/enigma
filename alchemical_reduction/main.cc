9ab3866582b1cf1999de36953dd9973ec47ef7c3
Pedro Schneider <pedroschneider@outlook.com>
2020-09-18 21:41:29 -0300

    adapt part I

--- alchemical_reduction/main.cc
+++ alchemical_reduction/main.cc
@@ -1,22 +1,32 @@
 
 #include <iostream>
 #include <stack>
+#include <vector>
+
+using namespace std;
 
 int main() {
-  std::stack<char> st;
-  for (char ch; scanf("%c", &ch) != EOF;) {
-    if (st.size() == 0) {
-      st.push(ch);
-      continue;
+
+  auto react = [&] (vector<char> v) {
+    stack<char> st;
+    for (char ch; scanf("%c", &ch) != EOF;) {
+      if (st.size() == 0) {
+        st.push(ch);
+        continue;
+      }
+
+      if ((st.top() == toupper(ch) || st.top() == tolower(ch)) && st.top() != ch)
+        st.pop();
+      else
+        st.push(ch);
+
     }
+    return st.size() - 1;
+  };
 
-    if ((st.top() == toupper(ch) || st.top() == tolower(ch)) && st.top() != ch)
-      st.pop();
-    else
-      st.push(ch);
+  vector<char> v;
+  for (auto& e : v) cin >> e;
 
-  }
-
-  printf("%ld\n", st.size()-1);
+  printf("%ld\n", react(v));
   return 0;
 }
