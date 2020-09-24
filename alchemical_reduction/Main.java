e0fc0f80f65b6d2dc46fbb50c7e2edfc343ca474
William <pedroschneider@outlook.com>
2020-09-24 15:30:00 -0300

    add java version + minor tweaks

---
+++ alchemical_reduction/Main.java
@@ -0,0 +1,55 @@
+
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.HashSet;
+
+public class Main {
+
+    private static int react(char[] polymer, char skip) {
+        var st = new Stack<Character>();
+
+        for (var unit : polymer) {
+            var up = Character.toUpperCase(unit);
+            var lo = Character.toLowerCase(unit);
+
+            if (up == skip || lo == skip) continue;
+
+            if (st.empty()) {
+                st.push(unit);
+                continue;
+            }
+
+            if (unit != st.peek() && (st.peek() == up || st.peek() == lo)) {
+                st.pop();
+            } else {
+                st.push(unit);
+            }
+        }
+
+        return st.size();
+    }
+
+    public static void main(String[] args) {
+        // lend o input
+        var in = new Scanner(System.in);
+        var polymer = in.nextLine().toCharArray();
+        in.close();
+
+        // part I
+        int resp = react(polymer, '-');
+        System.out.println(resp);
+
+        // part II
+        int minimum = resp;
+        var seen = new HashSet<Character>();
+        for (var unit : polymer) {
+            if (seen.contains(unit)) continue;
+
+            seen.add(unit);
+            minimum = Math.min(react(polymer, unit), minimum);
+        }
+
+        System.out.println(minimum);
+    }
+
+}
--- alchemical_reduction/run.sh
+++ alchemical_reduction/run.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
+# java version
+# javac Main.java && java Main < 0.in
+
+# python version
+# python3 reactions.py
+
 g++ -std=c++17 -O3 main.cc && ./a.out < 0.in && rm a.out
